60ca558797fb596321665a6d0c169ddd1a650d28
Anton Vasylkov <neatrabe345@gmail.com>
2024-09-24 14:50:25 +0200

    Base Health Check

---
+++ main_controller_module/src/test/java/me/vasylkov/main_controller_module/MainControllerModuleApplicationTests.java
@@ -0,0 +1,15 @@
+package me.vasylkov.main_controller_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainControllerModuleApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
