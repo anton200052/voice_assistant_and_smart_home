4b9d0981a8529043a095ee46a10d440c2f3a0d61
Anton Vasylkov <neatrabe345@gmail.com>
2024-09-25 00:08:11 +0200

    connect to main module

--- voice_recogniton_module/src/data_senders/data_sender.py
+++ voice_recogniton_module/src/data_senders/data_sender.py
@@ -1,6 +1,15 @@
-def text_sender_imitation_func(text):
-    print("Отправляю текст:" + text)
+import requests
 
 
-def notification_sender_imitation_func(text):
-    print("Отправляю текст:" + text)
+def sendRecognizedText(text):
+    url = 'http://localhost:8080/recognized-text'
+    data = {
+        'text': text,
+    }
+
+    response = requests.post(url, json=data)
+
+    if response.status_code == 200:
+        print("Ответ:", response.json())
+    else:
+        print("Ошибка: {response.status_code}")
